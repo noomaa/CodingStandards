6faa3494a618b51627a939463582b67a956d09cb
IamProgrammerLK <iamprogrammerlk@gmail.com>
2022-04-19 20:50:30 +0530

    sum tests

--- Noomaa/Sniffs/WhiteSpace/FunctionClosingBraceSpaceSniff.php
+++ Noomaa/Sniffs/WhiteSpace/FunctionClosingBraceSpaceSniff.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Checks that there is one empty line before the closing brace of a function.
+ * Checks that there is no empty line before the closing brace of a function.
  */
 
 namespace Noomaa\CodingStandards\WhiteSpace;
@@ -25,119 +25,165 @@ class FunctionClosingBraceSpaceSniff implements Sniff
         ];
     }
 
-    public function process( File $file, $position )
+    public function process(File $file, $position): void
     {
         $tokens = $file->getTokens();
 
-        if( isset( $tokens[ $position ][ 'scope_closer' ] ) === false )
-        {
-            // Probably an interface method.
-            return;
-        }
+        // if( isset( $tokens[ $position ][ 'scope_closer' ] ) === false )
+        // {
+        //     // Probably an interface method.
+        //     return;
+        // }
 
-        $closeBrace  = $tokens[$position]['scope_closer'];
-        $prevContent = $file->findPrevious(T_WHITESPACE, ($closeBrace - 1), null, true);
+        $close_brace_position   = $tokens[$position]['scope_closer'];
+        $previous_content       = $file->findPrevious(T_WHITESPACE, ($close_brace_position - 1), null, true);
+        $close_brace_line       = $tokens[$close_brace_position]['line'];
+        $previous_line          = $tokens[$previous_content]['line'];
+        $founds                 = ($close_brace_line - $previous_line - 1);
 
-        // Special case for empty JS functions.
-        if( $file->tokenizerType === 'JS' && $prevContent === $tokens[$position]['scope_opener'] )
-        {
-            // In this case, the opening and closing brace must be
-            // right next to each other.
-            if( $tokens[$position]['scope_closer'] !== ($tokens[$position]['scope_opener'] + 1) )
+        // $nestedFunction = false;
+        // if( $file->hasCondition($position, [T_FUNCTION, T_CLOSURE]) === true || isset($tokens[$position]['nested_parenthesis']) === true )
+        // {
+        //     $nestedFunction = true;
+        // }
+
+
+        // if( $nestedFunction === true )
+        // {
+        //     // if ( $founds < 0 ) {
+        //     //     $error = 'Closing brace of nested function must be on a new line';
+        //     //     $fix   = $file->addFixableError($error, $close_brace_position, 'ContentBeforeClose');
+        //     //     if ( $fix === true ) {
+        //     //         $file->fixer->addNewlineBefore($close_brace_position);
+        //     //     }
+        //     // } elseif ( $founds > 0 ) {
+        //     //     $error = 'Expected 0 blank lines before closing brace of nested function; %s founds';
+        //     //     $data  = [$founds];
+        //     //     $fix   = $file->addFixableError($error, $close_brace_position, 'SpacingBeforeNestedClose', $data);
+
+        //     //     if ( $fix === true ) {
+        //     //         $file->fixer->beginChangeset();
+        //     //         $changeMade = false;
+        //     //         for ( $i = ($previous_content + 1); $i < $close_brace_position; $i++ ) {
+        //     //             // Try and maintain indentation.
+        //     //             if ( $tokens[$i]['line'] === ($close_brace_line - 1) ) {
+        //     //                 break;
+        //     //             }
+
+        //     //             $file->fixer->replaceToken($i, '');
+        //     //             $changeMade = true;
+        //     //         }
+
+        //     //         // Special case for when the last content contains the newline
+        //     //         // token as well, like with a comment.
+        //     //         if ( $changeMade === false ) {
+        //     //             $file->fixer->replaceToken(($previous_content + 1), '');
+        //     //         }
+
+        //     //         $file->fixer->endChangeset();
+        //     //     }//end if
+        //     // }//end if
+        // }
+        // else
+        // {
+
+            if( $founds !== 0 )
             {
-                $error = 'The opening and closing braces of empty functions must be directly next to each other; e.g., function () {}';
-                $fix   = $file->addFixableError($error, $closeBrace, 'SpacingBetween');
-                if( $fix === true )
-                {
-                    $file->fixer->beginChangeset();
-                    for( $i = ($tokens[$position]['scope_opener'] + 1); $i < $closeBrace; $i++ )
-                    {
-                        $file->fixer->replaceToken($i, '');
-                    }
+                $error = $founds .  ' ' . $previous_content . ' ' . $close_brace_position . 'NoomaaCodingStandard: Expected 0 blank line before closing function brace; %s founds';
+                $data  = [ $founds ];
+                $fix   = $file->addFixableError( $error, $close_brace_position, 'NoBlankLineBeforeClosingBrace', $data );
+                // if( $fix === true )
+                // {
+                    // if( $founds < 0 )
+                    // {
+                    //     $file->fixer->beginChangeset();
+                    //     for ( $i = ($previous_content + 1); $i < ($close_brace_position - 1); $i++ ) {
+                    //         $file->fixer->replaceToken($i, '');
+                    //     }
 
-                    $file->fixer->endChangeset();
-                }
+                    //     $file->fixer->replaceToken($i, $file->eolChar);
+                    //     $file->fixer->endChangeset();
+                    // } 
+
+
+
+                    // if( $founds > 0 )
+                    // {
+                    //     $file->fixer->beginChangeset();
+                    //     for( $i = ( $previous_content + 1 ); $i < ( $close_brace_position - 1 ); $i++ )
+                    //     {
+                    //         // $file->fixer->replaceToken( $i, '' );
+                    //         // $file->fixer->addNewline($close_brace_position);
+                    //         // $file->fixer->replaceToken( $i, $file->eolChar );
+                    //     }
+                    //     $file->fixer->endChangeset();
+
+                    // }
+                    // else
+                    // {
+                        // // Try and maintain indentation.
+                        // if ( $tokens[($close_brace_position - 1)]['code'] === T_WHITESPACE ) {
+                        //     $file->fixer->addNewline($close_brace_position - 1);
+                        // } else {
+                        //     $file->fixer->addNewline($close_brace_position);
+                        // }
+                // }
             }
 
-            return;
-        }
+                // if( $founds < 0 )
+                // {
+                //     $founds = 0;
+                // }
 
-        $nestedFunction = false;
-        if ( $file->hasCondition($position, [T_FUNCTION, T_CLOSURE]) === true
-            || isset($tokens[$position]['nested_parenthesis']) === true
-        ) {
-            $nestedFunction = true;
-        }
+            // if( $founds > 0 )
+            // {
+                // if( $founds < 0 )
+                // {
+                //     $founds = 0;
+                // // }
+                // $error = $founds . 'NoomaaCodingStandard: Expected 0 blank line before closing function brace; %s founds';
+                // $data  = [ $founds ];
+                // $fix   = $file->addFixableError( $error, $close_brace_position, 'NoBlankLineBeforeClosingBrace', $data );
 
-        $braceLine = $tokens[$closeBrace]['line'];
-        $prevLine  = $tokens[$prevContent]['line'];
-        $found     = ($braceLine - $prevLine - 1);
+                // if( $fix === true )
+                // {
+                //     // if( $founds < 0 )
+                //     // {
+                //     //     $file->fixer->beginChangeset();
+                //     //     for ( $i = ($previous_content + 1); $i < ($close_brace_position - 1); $i++ ) {
+                //     //         $file->fixer->replaceToken($i, '');
+                //     //     }
 
-        if ( $nestedFunction === true ) {
-            if ( $found < 0 ) {
-                $error = 'Closing brace of nested function must be on a new line';
-                $fix   = $file->addFixableError($error, $closeBrace, 'ContentBeforeClose');
-                if ( $fix === true ) {
-                    $file->fixer->addNewlineBefore($closeBrace);
-                }
-            } elseif ( $found > 0 ) {
-                $error = 'Expected 0 blank lines before closing brace of nested function; %s found';
-                $data  = [$found];
-                $fix   = $file->addFixableError($error, $closeBrace, 'SpacingBeforeNestedClose', $data);
+                //     //     $file->fixer->replaceToken($i, $file->eolChar);
+                //     //     $file->fixer->endChangeset();
+                //     // } 
 
-                if ( $fix === true ) {
-                    $file->fixer->beginChangeset();
-                    $changeMade = false;
-                    for ( $i = ($prevContent + 1); $i < $closeBrace; $i++ ) {
-                        // Try and maintain indentation.
-                        if ( $tokens[$i]['line'] === ($braceLine - 1) ) {
-                            break;
-                        }
 
-                        $file->fixer->replaceToken($i, '');
-                        $changeMade = true;
-                    }
 
-                    // Special case for when the last content contains the newline
-                    // token as well, like with a comment.
-                    if ( $changeMade === false ) {
-                        $file->fixer->replaceToken(($prevContent + 1), '');
-                    }
+                //     if( $founds > 0 )
+                //     {
+                //         $file->fixer->beginChangeset();
+                //         for( $i = ( $previous_content + 1 ); $i < ( $close_brace_position - 1 ); $i++ )
+                //         {
+                //             $file->fixer->replaceToken( $i, '' );
+                //         }
+                //         $file->fixer->replaceToken( $i, $file->eolChar );
+                //         $file->fixer->endChangeset();
 
-                    $file->fixer->endChangeset();
-                }//end if
+                //     }
+                //     else
+                //     {
+                //         // // Try and maintain indentation.
+                //         // if ( $tokens[($close_brace_position - 1)]['code'] === T_WHITESPACE ) {
+                //         //     $file->fixer->addNewline($close_brace_position - 1);
+                //         // } else {
+                //         //     $file->fixer->addNewline($close_brace_position);
+                //         // }
+                //     }
+                // }
             }//end if
-        } else {
-            if ( $found !== 1 ) {
-                if ( $found < 0 ) {
-                    $found = 0;
-                }
+        // }//end if
 
-                $error = 'Expected 1 blank line before closing function brace; %s found';
-                $data  = [$found];
-                $fix   = $file->addFixableError($error, $closeBrace, 'SpacingBeforeClose', $data);
-
-                if ( $fix === true ) {
-                    if ( $found > 1 ) {
-                        $file->fixer->beginChangeset();
-                        for ( $i = ($prevContent + 1); $i < ($closeBrace - 1); $i++ ) {
-                            $file->fixer->replaceToken($i, '');
-                        }
-
-                        $file->fixer->replaceToken($i, $file->eolChar);
-                        $file->fixer->endChangeset();
-                    } else {
-                        // Try and maintain indentation.
-                        if ( $tokens[($closeBrace - 1)]['code'] === T_WHITESPACE ) {
-                            $file->fixer->addNewline($closeBrace - 1);
-                        } else {
-                            $file->fixer->addNewline($closeBrace);
-                        }
-                    }
-                }
-            }//end if
-        }//end if
-
-    }
+    // }
 
 }
